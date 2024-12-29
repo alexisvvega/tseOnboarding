3a9f474e71c0920a4b59b1934cb818bc67e21374
alexisvvega <a9vega@ucsd.edu>
2024-12-28 23:16:38 -0800

    part 2.1 complete

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -8,9 +8,7 @@ import cors from "cors";
 import { isHttpError } from "http-errors";
 import taskRoutes from "src/routes/task";
 import tasksRoutes from "src/routes/tasks";
-import userRoutes from "./routes/user";
-
-
+import userRoutes from "../src/routes/user";
 
 const app = express();
 
